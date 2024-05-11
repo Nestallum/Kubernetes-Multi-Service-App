569f3d774ee41cf4a3d54eaf78f7154a84cb111d
Nestallum <nassimlattab@gmail.com>
2024-05-12 01:51:59 +0200

    petite modification des noms des méthodes

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -3,17 +3,16 @@ import pandas as pd
 from flask import Flask
 
 app = Flask(__name__)
-SERVICE1_URL = "http://back-end-service:80"
+BACKEND_SERVICE_URL = "http://back-end-service:80"
 
 def generate_html_from_dataframe(df):
     html = "<h1>Student Data</h1>"
     html += df.to_html(index=False)
     return html
 
-
 @app.route('/')
 def get_data_from_backend():
-    response = requests.get(SERVICE1_URL)
+    response = requests.get(BACKEND_SERVICE_URL)
     data = response.json()
     df = pd.DataFrame(data)
     return generate_html_from_dataframe(df)
